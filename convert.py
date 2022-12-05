16cdbcaab26cfc8543bdcf1f36b6084cedcbc8ac
Nobukazu Ishii <nobnov@users.noreply.github.com>
2022-12-06 01:54:30 +0900

    fix

--- convert.py
+++ convert.py
@@ -10,8 +10,14 @@ for filename in sorted(glob.glob("images/*.jpg")):
     img_array.append(img)
 
 name = 'project.mp4'
-out = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*'MP4V'), 5.0, size)
+out = cv2.VideoWriter(name,
+                      cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
+                      5.0,
+                      size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
+
+print("Finish")
+
